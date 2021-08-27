040db0a74f7908ec204cbf81a3ffac135bf3170b
Rohit Gandhi <rohit.r.gandhi@gmail.com>
2021-08-27 16:46:11 +0530

    Done

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
-var dob=document.getElementById("date-of-birth");
+//taking variables from HTML world to Javascript world
+const dob=document.querySelector("#date-of-birth");
+const luckyNumber=document.querySelector("#lucky-number");
+const output=document.querySelector("#output");
+const checkButton=document.querySelector("#button");
 
-var luckyNumber=document.getElementById("lucky-number");
-
-var output=document.getElementById("output");
-
-var checkButton=document.getElementById("button");
+//when button is clicked check function is called.
+checkButton.addEventListener("click", check);
 
 function check()
 {
     let birthDate=dob.value;
     let sum=calculateSum(birthDate);
-    lucky(sum);
-}
-function calculateSum(birthDate)
-{
-    birthDate=birthDate.replaceAll("/","");
-    let quotient=0;
-    let remainder=0;
-    let dateAdd=0;
-    
-    for(let i=0;i<birthDate.length;i++) 
+    //this is where it checks for luck
+    if (sum%luckyNumber.value===0)
     {
-        quotient=birthDate/10;
-        remainder=birthDate%10;
-        dateAdd= dateAdd+remainder;
+        output.innerText="Your Birthday is lucky";
     }
-    return dateAdd;
-}
-
-checkButton.addEventListener("click", check);
-
-
-function lucky(){
-    if (dob.value%luckyNumber.value===0)
+    else
     {
-        output.innerText="Birthday is lucky"
-    }
-    else{
-     output.innerText="Sorry but your Birthday is not lucky"
+        output.innerText="You have to make your OWN luck";
     }
 }
+function calculateSum(birthDate) //returns addition of digits
+{   
+    //this removes dashes to make it a string of numbers
+    birthDate=birthDate.replaceAll("-","");
+
+    // addition of numbers takes place here
+    let add=0;
+    for(let i=0;i<birthDate.length;i++)
+    {
+        add=add+Number(birthDate.charAt(i)); //birthDate is a string and cannot be added to add hence converted to a number.
+    }
+    return add;
+}
+
