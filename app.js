f51476562af7b2c11feb3f9f0470ad3d85ef0f57
Rohit Gandhi <rohit.r.gandhi@gmail.com>
2021-09-26 16:38:51 +0530

    fix:no input error message

--- app.js
+++ app.js
@@ -4,34 +4,38 @@ const luckyNumber=document.querySelector("#lucky-number");
 const output=document.querySelector("#output");
 const checkButton=document.querySelector("#button");
 
-//when button is clicked check function is called.
-checkButton.addEventListener("click", check);
-
 function check()
 {
     let birthDate=dob.value;
     let sum=calculateSum(birthDate);
-    //this is where it checks for luck
-    if (sum%luckyNumber.value===0)
+
+    
+    if(sum && luckyNumber) // this checks whether both inputs are filled
     {
-        output.innerText="Your Birthday is lucky";
+        if (sum%luckyNumber.value===0) //this is where it checks for luck
+        {
+            output.innerText="Your Birthday is lucky";
+        }
+        else
+        {
+            output.innerText= "You have to make your OWN luck";
+        }
     }
-    else
-    {
-        output.innerText="You have to make your OWN luck";
-    }
+    else output.innerText= "How to calculate luck without input?";
 }
 function calculateSum(birthDate) //returns addition of digits
 {   
-    //this removes dashes to make it a string of numbers
-    birthDate=birthDate.replaceAll("-","");
+    
+    birthDate=birthDate.replaceAll("-",""); //this removes dashes to make it a string of numbers
 
-    // addition of numbers takes place here
+    
     let add=0;
-    for(let i=0;i<birthDate.length;i++)
+    for(let i=0;i<birthDate.length;i++) // addition of numbers takes place here
     {
-        add=add+Number(birthDate.charAt(i)); //birthDate is a string and cannot be added to add hence converted to a number.
+        add=add+Number(birthDate.charAt(i)); //birthDate is a string and cannot be added to add hence converted to a number using "Number()".
     }
     return add;
 }
 
+//when button is clicked "check" function is called.
+checkButton.addEventListener("click", check);
